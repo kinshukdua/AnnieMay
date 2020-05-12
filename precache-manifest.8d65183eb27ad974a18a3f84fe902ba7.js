9451044d8f4075ddd6d4fd8bc8350aac277db153
Kinshuk Dua <kinshukduaexam@gmail.com>
2020-05-12 19:09:18 +0530

    Updates

---
+++ precache-manifest.8d65183eb27ad974a18a3f84fe902ba7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef557ae9ac3ce8b72148d845263335d5",
+    "url": "/AnnieMay/index.html"
+  },
+  {
+    "revision": "9c7b19566c92c713cfac",
+    "url": "/AnnieMay/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "22178a177c91519012d6",
+    "url": "/AnnieMay/static/css/main.212e407b.chunk.css"
+  },
+  {
+    "revision": "9c7b19566c92c713cfac",
+    "url": "/AnnieMay/static/js/2.676087fa.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/AnnieMay/static/js/2.676087fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22178a177c91519012d6",
+    "url": "/AnnieMay/static/js/main.a2d8a13f.chunk.js"
+  },
+  {
+    "revision": "fa7fa4f636defc9c2a82",
+    "url": "/AnnieMay/static/js/runtime-main.ee1264ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AnnieMay/static/media/logo.5d5d9eef.svg"
+  }
+]);
