a6f135a56efe5f2be46701c8138c7bd6f7b9e03c
Kinshuk Dua <kinshukduaexam@gmail.com>
2020-05-15 22:28:16 +0530

    remake ui from scratch w/o bootstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import button from 'react-bootstrap/button';
 import data from './data';
 import Suggestion from './suggestion'
 import Navbar from 'react-bootstrap/Navbar'
@@ -31,65 +31,55 @@ class App extends React.Component {
     const p = this.state.prev_id;
     this.setState({"prev_id":null,"id":p,"q_no":this.state.q_no-1})
   }
+
+  handleReset()
+  {
+    this.setState({"prev_id":null,"id":0,"q_no":1})
+  }
   
 
   render()
   {
     var back_avail;
+    var reset_avail = !this.state.id ? "none" : "inline-block"; 
     if (this.state.prev_id == null)
     {
       back_avail = "none";
     }
     else
     {
-      back_avail = "block";
+      back_avail = "inline-block";
     }
     var list_of_buttons = [];
     if (data[this.state.id].children.length > 1) {
       for ( var child_id of data[this.state.id].children)
       {
         list_of_buttons.push(
-        <Button key={child_id} onClick={this.handleClick.bind(this, child_id)} className="btn-block"  
-        variant="danger" style = {{margin: "10px auto"}}>{data[child_id].text}
-        </Button>
-
-          )
+        <div>
+          <button className='option' key={child_id} onClick={this.handleClick.bind(this, child_id)}>
+           {data[child_id].text}
+          </button>
+          <br />
+        </div>)
       }
         
         return (
-        <div> 
-          <Navbar variant="dark" bg='dark' className="justify-content-center">
-
-            <Navbar.Brand>
-              <img
-                alt=""
-                src="https://seeklogo.com/images/F/Fullmetall_Alchemist_anime-logo-59840B9E1F-seeklogo.com.png"
-                width="30px"
-                height="30px"
-                className="d-inline-block align-top"
-              />   {'\t'}      
-                          Annie May's Opinions
-            </Navbar.Brand>
-            
-          </Navbar>
-          
-          <div className='align-item-center' style = {{display: 'block', textAlign:"center"}}>
-            <Card border = 'danger' style={{ margin:'25px', width: 'fit-content'}} 
-              className='shadow-lg p-3 mb-5 bg-white rounded mx-auto'>
-            <Card.Body>
-              <Card.Title style = {{fontSize: "45px"}}>{data[this.state.id].text}</Card.Title>
+        <div className='q_container'> 
+              <h1 id ="question">{data[this.state.id].text}</h1>
               <hr />
-              <Card.Subtitle className="mb-2 text-muted">Question no. {this.state.q_no}</Card.Subtitle>
-              <div  className="btn-group-justified btn-group-lg">
+              <p>Question no. {this.state.q_no}</p>
+              <div  className="btn-container">
                 {list_of_buttons}
               </div>
-              <Button className = 'brk-btn' onClick={this.handleBack.bind(this)} style = {{display:back_avail,float: "left",  borderRadius:"0%"}}><i class="fas fa-arrow-left"></i></Button>
-            </Card.Body>
-            </Card>
-          </div>
-          <Navbar className='py-0' fixed='bottom' bg = 'dark' variant='dark'>
-            <Navbar.Text>Made by Kinshuk Dua, 2020</Navbar.Text>
-          </Navbar>
+              <br />
+              <button className = 'option-btn' onClick={this.handleBack.bind(this)} 
+              style = {{display:back_avail,float: "left"}}>
+                <i class="fas fa-arrow-left fa-2x"></i>
+              </button>
+              <button className = 'option-btn' onClick={this.handleReset.bind(this)} 
+              style = {{display:reset_avail,float: "right"}}>
+                <i class="fas fa-undo fa-2x"></i>
+              </button>
         </div>
         );
     }
